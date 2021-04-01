db2619a48f172345227f84b300f831f9275a0af1
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-04-01 19:31:13 -0400

    corrected sweepstakes class and dictionary

--- sweepstakes.py
+++ sweepstakes.py
@@ -1,16 +1,21 @@
+from contestant import Contestant
+
 class Sweepstakes:
         def __init__(self, name):
-            self.dictionary_contestant = {
-                "First name": input("Please enter your first name"),
-                "Last name": input("Please enter your last name"),
-                "Email Address": input("Please enter your email for results"),
-                "Registration number": input("Please provide your email")
+            self.contestants = {
+                "First name": 'First name',
+                "Last name": 'Last name',
+                "Email Address": '@.com',
+                "Registration number": ''
             }
+            # self.contestants = {}
+            # each item in your dictionary is a key value pair with:
+            # unique registration number: Contestant() object
 
 
         def register_contestant(self):
             response = input('Would you like to register?')
-            self.dictionary_contestant = response
+            self.contestants= response
             print(f'You are now registered! {self.registration_number}')
 
         def pick_winner(self, contestant):
