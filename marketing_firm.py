67a304981a3c207fe3b444716b8bfca9a0dc39af
Sebastian <maxwellsebas@gmail.com>
2021-06-11 18:25:54 -0400

    updated marketing firm

--- marketing_firm.py
+++ marketing_firm.py
@@ -1,6 +1,10 @@
+from marketing_firm_creator import MarketingFirmCreator
+
 class MarketingFirm:
-    def __init__(self, manager):
-        pass
+
+    def __init__(self):
+        self.manager = MarketingFirmCreator()
 
     def create_sweepstakes(self):
-        
+        sweep = self.manager.choose_manager()
+        return sweep
